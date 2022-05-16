d851c6a7383f0cc93907e1412cc8ef2b01488995
SahithaReddy <sahithareddy2000@gmail.com>
2022-05-16 18:11:18 +0530

    first-commit

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "region" {
+    default = "ap-south-1"
+}
+
+variable "bucket_name" {
+    default = "terraformb817504"
+}
+
+variable "acl_value" {
+    default = "private"
+}
---
+++ vars.tf
@@ -0,0 +1,20 @@
+variable "AWS_REGION" {
+  default = "ap-south-1"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "ubuntu.pem"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    ap-south-1 = "ami-0756a1c858554433e"
+    us-west-2 = "ami-0756a1c858554433e"
+    eu-west-1 = "ami-0756a1c858554433e"
+  }
+}
